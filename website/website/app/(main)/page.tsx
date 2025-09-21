e9bab5d612a2969ac72070ba45ad0b242e77a4db
gazi-faysal-jubayer <gazi.faysal.jubayer@gmail.com>
2025-09-21 22:39:31 +0600

    web file structure fix

---
+++ website/website/app/(main)/page.tsx
@@ -0,0 +1,24 @@
+import { HeroSection } from '@/components/home/hero-section'
+import { StatsCounter } from '@/components/home/stats-counter'
+import { FeaturedDiscoveries } from '@/components/home/featured-discoveries'
+import { QuickSearch } from '@/components/home/quick-search'
+import { GalaxyMapPreview } from '@/components/home/galaxy-map-preview'
+import { NewsTicker } from '@/components/home/news-ticker'
+import { CTASection } from '@/components/home/cta-section'
+import Hero from '@/components/home/hero'
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      {/* <HeroSection /> */}
+      <StatsCounter />
+      <FeaturedDiscoveries />
+      <QuickSearch />
+      <GalaxyMapPreview />
+      <NewsTicker />
+      <CTASection />
+    </>
+  )
+}
+
---
+++ website/website/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  basePath: '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
+
