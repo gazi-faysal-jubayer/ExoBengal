9e770c8eca250a25840d8c04b83d9f062de73f74
gazi-faysal-jubayer <gazi.faysal.jubayer@gmail.com>
2025-09-21 23:06:10 +0600

    deploy 1.002

--- website/next.config.js
+++ website/next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : ''
+const repoBasePath = isProd && isGithubActions && repoName ? `/${repoName}` : ''
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  basePath: '',
-  assetPrefix: '',
+  basePath: repoBasePath,
+  assetPrefix: repoBasePath ? `${repoBasePath}/` : '',
   images: {
     unoptimized: true,
   },
