60c47f868f4751a79827c5b92b794e47254925cc
gazi-faysal-jubayer <gazi.faysal.jubayer@gmail.com>
2025-09-27 10:38:27 +0600

    space gap between header and news bar fixed

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
         <Providers>
           <div className="flex min-h-screen flex-col">
             <Header />
-            <NewsBar className="sticky top-16 z-40 -mt-1" />
+            <NewsBar className="sticky top-16 z-40 -mt-16" />
             <main className="flex-1">
               {children}
             </main>
